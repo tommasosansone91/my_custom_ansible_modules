39fc1e02579fcfb0eb12f3755764cb097a94935f
tommasosansone91 <tommaso.sansone91@virgilio.it>
2023-11-09 10:38:07 +0100

    added gear to catch code injection and improved errors catch

--- ansible_custom_module__append_multiple_csv.py
+++ ansible_custom_module__append_multiple_csv.py
@@ -21,7 +21,7 @@ def run_module():
         path_of_csv_to_be_updated = dict(type='str', required=True),
 
         # path of the csv files to be appended
-        directory_of_csvs_to_be_appended: = dict(type='str', required=True),
+        directory_of_csvs_to_be_appended = dict(type='str', required=True),
 
         # common pattern in filename of the csv files to be appended
         filename_common_pattern_of_csvs_to_be_appended = dict(type='str', required=True),
@@ -68,7 +68,7 @@ def run_module():
 
     path_csv_updating = module.params['path_of_csv_to_be_updated']
 
-    dir_all_csv_appending = module.params['directory_of_csvs_to_be_appended:']
+    dir_all_csv_appending = module.params['directory_of_csvs_to_be_appended']
 
     filename_pattern = module.params['filename_common_pattern_of_csvs_to_be_appended']
 
@@ -78,20 +78,24 @@ def run_module():
     #--------------
 
     # check filename_pattern input is not injection
-    # protection form injection [^a-zA-Z0-9^*_-] detect char which are no letter numbers - _ and *
-    bad_pattern = re.compile("[^a-zA-Z0-9^*_-]")
+    # protection form injection [^a-zA-Z0-9^*_-] detect char which are no letter numbers - / _ and *
+    bad_pattern = re.compile("[^a-zA-Z0-9^*_\/-]")
 
     if  bad_pattern.match(filename_pattern):
         print("Invalid input! Execution aborted!\n'filename_common_pattern_of_csvs_to_be_appended' can only contain number, letters, dash, underscore and asterisk.\nYou gave me: {}".format(filename_pattern))
         exit(1)
 
     # check file path input is not injection
-    # protection form injection [^a-zA-Z0-9^_-] detect char which are no letter numbers - and _
-    bad_pattern = re.compile("[^a-zA-Z0-9^._-]")
+    # protection form injection [^a-zA-Z0-9^_-] detect char which are no letter numbers - / and _
+    bad_pattern = re.compile("[^a-zA-Z0-9^._\/-]")
 
     if  bad_pattern.match(dir_all_csv_appending):
-        print("Invalid input! Execution aborted!\n'directory_of_csvs_to_be_appended:' can only contain number, letters, dash and underscore.\nYou gave me: {}".format(filename_pattern))
-        exit(1)
+        print("Invalid input! Execution aborted!\n'directory_of_csvs_to_be_appended' can only contain number, letters, dash and underscore.\nYou gave me: {}".format(dir_all_csv_appending))
+        exit(1)       
+
+    if  bad_pattern.match(path_csv_updating):
+        print("Invalid input! Execution aborted!\n'path_of_csv_to_be_updated' can only contain number, letters, dash and underscore.\nYou gave me: {}".format(path_csv_updating))
+        exit(1)   
 
     # create find bash command
 
@@ -100,14 +104,35 @@ def run_module():
     # lista_test=$(find . -maxdepth 1 -name "*test2*")
 
     find_command = "find"
-    find_arguments = [ path_all_csv_appending, "-maxdepth", "1", "-name", "*{}*".format(filename_pattern) ]    
+    find_arguments = [ dir_all_csv_appending, "-maxdepth", "1", "-name", "*{}*".format(filename_pattern) ]    
 
     # launch the bash commands and get the list fo files
     # subprocess.run(["ls", "-l"])  # doesn't capture output
 
-    find_result = subprocess.run([find_command] + find_arguments, capture_output=True, text=True)
-    list_of_relpaths_appending_csvs = find_result.stdout.strip().split("\n")
-    list_of_relpaths_appending_csvs.sort()
+    # supported only by python 3.5
+    # find_result = subprocess.run([find_command] + find_arguments, capture_output=True, text=True)
+
+    # workaround to for python2
+    find_process = subprocess.Popen([find_command] + find_arguments, stdout=subprocess.PIPE)
+    find_result, _ = find_process.communicate()
+    find_result = find_result.decode("utf-8")
+
+    try:
+        # for python 3.5+
+        # list_of_relpaths_appending_csvs = find_result.stdout.strip().split("\n") 
+
+        list_of_relpaths_appending_csvs = find_result.strip().split("\n")
+        
+        if path_csv_updating in list_of_relpaths_appending_csvs:
+            list_of_relpaths_appending_csvs.remove(path_csv_updating)
+        
+        list_of_relpaths_appending_csvs.sort()
+
+
+
+    except Exception as e:
+        raise Exception("Exception when extraction the find results:\nfind_result: \n{}\n\nfind result type:{}".format(find_result, type(find_result) ))
+
 
 
     # append sequentially each csv
@@ -127,7 +152,7 @@ def run_module():
 
                 for row in reader:
                     writer.write(row)
-                    print("Appending {}: Added row: {}".format(path_csv_appending, row))
+                    # print("Appending {}: Added row: {}".format(path_csv_appending, row))
 
             with open(path_csv_updating, 'r') as reader:
                 row_count_after = sum(1 for row in reader)        
